c99272ce046a23bdc76e86803d5438e60a7a1d64
Suntipap <suntipap1312@gmail.com>
2021-08-05 19:25:59 +0700

    initial

---
+++ Excercise4_Suntipap_A.py
@@ -0,0 +1,10 @@
+FoundationEnglish = FE = 72
+GeneralEnglish = GE = 61.9
+IntroductionToComputerSystems = ICS = 80.5
+ComputerProgramming = CP = 75
+
+print("----Your Score----")
+print("Foundation English :",FE )
+print("Foundation English :",GE )
+print("Foundation English :",ICS )
+print("Foundation English :",CP )
